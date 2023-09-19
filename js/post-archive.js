663871f3a1c764692f6b0ba04733a4b368b92804
subcryxos <kamyarfn@yahoo.com>
2023-09-19 15:07:45 -0700

    fix the post archive page

--- js/post-archive.js
+++ js/post-archive.js
@@ -1,9 +1,13 @@
 
-var swiper = new Swiper(".best-cities", {
-    slidesPerView:4,
-    spaceBetween: 20,
-     autoplay: {
-      delay: 20000000,
+const windowWidth = window.innerWidth;
+
+if (windowWidth <= 460) {
+
+  var swiper = new Swiper(".best-cities", {
+    slidesPerView: 2.1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2000,
       disableOnInteraction: false,
     },
 
@@ -12,5 +16,49 @@ var swiper = new Swiper(".best-cities", {
       prevEl: " .moving-arrow-r",
     },
 
-   
+
   });
+
+}
+
+if (windowWidth <= 1000) {
+
+  var swiper = new Swiper(".best-cities", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".moving-arrow-l",
+      prevEl: " .moving-arrow-r",
+    },
+
+
+  });
+
+}
+else {
+
+  var swiper = new Swiper(".best-cities", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".moving-arrow-l",
+      prevEl: " .moving-arrow-r",
+    },
+
+
+  });
+
+}
+
+
+
