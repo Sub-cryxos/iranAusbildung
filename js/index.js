be684fff6576cd3f4aa470ca98092df04eb17f85
subcryxos <kamyarfn@yahoo.com>
2023-09-25 07:51:04 -0700

    index page final

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ const windowWidth = window.innerWidth;
 if (windowWidth <= 612) {
 
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
+    slidesPerView: 1.5,
     spaceBetween: 10,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     }, autoplay: {
-      delay: 1000,
+      delay: 100000,
       disableOnInteraction: false,
     },
   });
@@ -32,10 +32,10 @@ if (windowWidth <= 612) {
 if (windowWidth <= 600) {
 
   var swiper = new Swiper(".best_ausbildungs", {
-    slidesPerView: 1.1,
+    slidesPerView: 1.08,
     spaceBetween: 20,
     autoplay: {
-      delay: 1000,
+      delay: 100000,
       disableOnInteraction: false,
     },
 
