7e5ff416d882b164feab2d1b19b8b28d062030ca
subcryxos <kamyarfn@yahoo.com>
2023-10-01 08:59:39 -0700

    additional index and compare and footer

--- js/compare.js
+++ js/compare.js
@@ -5,13 +5,19 @@ $(document).ready(function () {
     });
 
     $(' .right-compare_box .comapre-body .list-of-asubildungs li ').click(function (e) {
-        const valuePr = $(this).children('span:nth-child(1)').text();
-        const valueDe = $(this).children('span:nth-child(2)').text();
+        const valuePr = $(this).find("#persian_value").text();
+        const valueDe =  $(this).find("#germany_value").text();
+        console.log("log",valuePr)
+        console.log("log",valueDe)
+
         $(' .right-compare_box  .ausbildung-title #pr').text(valuePr);
         $(' .right-compare_box .ausbildung-title #de').text(valueDe);
         persianTitle = valuePr;
         $('.pr-t-t').text(valuePr)
+                $(".right-compare_box  .ausbildung-title h1").text(valuePr)
+        $(".right-compare_box  .ausbildung-title h2").text(valueDe)
 
+        
         $('.right-compare_box .comapre-body').removeClass('active-au-b');
     });
 
@@ -26,12 +32,15 @@ $(document).ready(function () {
 
 
     $(' .left-compare_box .comapre-body .list-of-asubildungs li ').click(function (e) {
-        const valuePr = $(this).children('span:nth-child(1)').text();
-        const valueDe = $(this).children('span:nth-child(2)').text();
+        const valuePr = $(this).find("#persian_value").text();
+        const valueDe =  $(this).find("#germany_value").text();
         $('.left-compare_box .ausbildung-title #pr').text(valuePr);
         $(' .left-compare_box .ausbildung-title #de').text(valueDe);
         persianTitle = valuePr;
         $(' .pl-t-t ').text(valuePr)
+
+        $(".left-compare_box .ausbildung-title h1").text(valuePr)
+        $(".left-compare_box .ausbildung-title h2").text(valueDe)
 
         $(' .left-compare_box .comapre-body').removeClass('active-au-b');
     });
