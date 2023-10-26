1e999c70e557cfed4d27e4a0f71ce0550752b7d3
subcryxos <kamyarfn@yahoo.com>
2023-10-26 11:18:27 -0700

    adding some features

--- js/Jobs.js
+++ js/Jobs.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-        
+       $('#filter-subject-select').click(function (e) { 
+        $('.subjects-f').toggleClass('filter-actives-subject');
+       });
+
+       $('#filter-periodic-time').click(function (e) { 
+        $('.periodic-time').toggleClass('periodic-time-activator');
+       });
+       
+       $('#filter-subject-type').click(function (e) { 
+        $('.types-f').toggleClass('types-f-activator');
+       });
+
+       $('#filter-work-time').click(function (e) { 
+        $('.f-period').toggleClass('periodic-time-activator');
+       });
 });
