a6176acf1eacccf1ee592934cc0fb84e1829fad3
subcryxos <kamyarfn@yahoo.com>
2023-11-15 12:18:16 -0800

    fix the ausbildung single

--- js/ausbildung-single.js
+++ js/ausbildung-single.js
@@ -11,4 +11,16 @@ $(document).ready(function () {
     $('.dislike').click(function (e) { 
         $(e.target).toggleClass('active-dislike ');
     });
+
+    const docheight = $(window).innerHeight();
+
+    if(docheight > 900 ) {
+        $('.mobile-access-list').addClass('mobile-access-list-900h');
+       
+    }
+
+    $('.mobile-access-list').click(function (e) { 
+        $('.mobile-access-list .access-list ').toggleClass('access-list-mobile-active ')
+        
+    });
 });
