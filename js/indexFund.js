db4e4a9b36ee01615a7b04b7ed009d9ac8031f76
subcryxos <kamyarfn@yahoo.com>
2023-11-17 06:10:42 -0800

    fix the header

--- js/indexFund.js
+++ js/indexFund.js
@@ -4,5 +4,24 @@ $(document).ready(function () {
       $(e.target).toggleClass('saved-ausbildung');
       
     });
+
+    $(window).scroll(function(){
+      var sticky = $('.sticky-header'),
+          scroll = $(window).scrollTop();
+    
+      if (scroll >=100) sticky.addClass('sticky-header-active');
+      else sticky.removeClass('sticky-header-active');
+    });
+
+    $('.side-activator').click(function (e) { 
+      $('.mobile-navbar').addClass('mobile-navbar-active');
+      
+    });
+
+    $('.mobile-header-close').click(function (e) { 
+      
+      $('.mobile-navbar').removeClass('mobile-navbar-active');
+      
+    });
   
   });   
