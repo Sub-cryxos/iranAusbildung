1a009303ee5b5286ae29c4243daef72e53780792
subcryxos <kamyarfn@yahoo.com>
2023-11-26 10:46:43 -0800

    initial final pages

--- js/indexFund.js
+++ js/indexFund.js
@@ -5,23 +5,5 @@ $(document).ready(function () {
       
     });
 
-    $(window).scroll(function(){
-      var sticky = $('.sticky-header'),
-          scroll = $(window).scrollTop();
-    
-      if (scroll >=100) sticky.addClass('sticky-header-active');
-      else sticky.removeClass('sticky-header-active');
-    });
-
-    $('.side-activator').click(function (e) { 
-      $('.mobile-navbar').addClass('mobile-navbar-active');
-      
-    });
-
-    $('.mobile-header-close').click(function (e) { 
-      
-      $('.mobile-navbar').removeClass('mobile-navbar-active');
-      
-    });
-  
+   
   });   
